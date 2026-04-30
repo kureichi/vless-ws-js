a87c003120a9f4ffdf569bf19c83d71966a8b65c
kureichi <hibikumonogatari@gmail.com>
2026-04-30 19:25:06 +0700

    feat: add trojan support

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import { VLESS } from './core/vless.js';
 import { UDPSocket } from './services/udpSocket.js';
 import { TCPSocket } from './services/tcpSocket.js';
 import { createLogger } from './logger/log.js';
+import { Proxy } from './core/proxy.js';
+import { TROJAN } from './core/trojan.js';
 
-const logger = createLogger({ scopes: ['main', 'server', 'vless', 'tcpSocket', 'udpSocket'] })
+const logger = createLogger({ scopes: ['main', 'server', 'proxy', 'vless', 'trojan', 'tcpSocket', 'udpSocket'] })
 
 const mainLogger = logger.getLogger('main')
 const PORT = process.env.PORT
@@ -21,14 +23,19 @@ server.on('upgrade', (request, socket, head) => {
 
 const tcpSocket = new TCPSocket(logger.getLogger('tcpSocket'))
 const udpSocket = new UDPSocket(logger.getLogger('udpSocket'))
+const vless = new VLESS(logger.getLogger('vless'))
+const trojan = new TROJAN(logger.getLogger('trojan'))
+
+const proxyInstance = new Proxy(logger.getLogger('proxy'), { vless, trojan })
+
 const wsLogger = logger.getLogger('server')
 wss.on('connection', (client) => {
   wsLogger.info('Incoming new connection')
 
-  const vless = new VLESS(client, logger.getLogger('vless'), { tcpSocket, udpSocket })
+  const proxy = proxyInstance.createProxy(client, { tcpSocket, udpSocket })
 
   client.on('message', (data) => {
-    vless.respond(data)
+    proxy.respond(data)
   });
 
   client.on('error', (error) => {
